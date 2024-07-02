b674476e1ca37c68d3203b5fa632294a5f2c2da9
vlimki <vlimki@proton.me>
2024-07-02 20:08:57 +0300

    backpropagation baby

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,14 +10,17 @@ module Lib
     , weights
     , biases
     , activate
+    , backProp
+    , calculateDelta
     ) where
 
 import Numeric.LinearAlgebra
-import Util (enumerate)
+import qualified Numeric.LinearAlgebra as LA
+import Util (enumerate, sigmoid, sigmoid')
 
-data Layer = Layer 
+data Layer = Layer
     { weights :: Matrix R
-    , biases :: Vector R
+    , biases :: Matrix R
     , sz:: Int
     , activation :: R -> R
     }
@@ -27,39 +30,45 @@ type Network = [Layer]
 initWeights :: Int -> Int -> Matrix R
 initWeights n m = (n><m) $ replicate (n*m) 0.123
 
-initBiases :: Int -> Vector R
-initBiases n = fromList $ replicate n 0.123
-
-sigmoid :: R -> R
-sigmoid x = 1 / (1 + exp (-x))
-
-sigmoid' :: R -> R
-sigmoid' x = sigmoid x * (1 - sigmoid x)
+initBiases :: Int -> Matrix R
+initBiases n = (n><1) $ replicate n 0.123
 
 -- layer size -> number of features
 sigmoidLayer :: Int -> Int -> Layer
 sigmoidLayer n m = Layer {weights=initWeights n m, biases=initBiases n, activation=sigmoid, sz=n}
 
-activate :: Layer -> Vector R -> Vector R
-activate l x = cmap (activation l) (weights l #> x + biases l)
+activate :: Layer -> Matrix R -> Matrix R
+activate l x = cmap (activation l) (weights l LA.<> x + biases l)
 
 initialize :: [Int] -> (R -> R) -> Network
-initialize s f = map (\x -> Layer {weights=initWeights 0 0, biases=fromList [], sz=x, activation=f}) s
+initialize s f = map (\x -> Layer {weights=initWeights 0 0, biases=initBiases 0, sz=x, activation=f}) s
 
--- TODO. The point of this function is to initialize the weight matrices for each layer to be suitable for the inputs
--- the feature number for a layer is the amount of outputs from the previous layer
-
--- feature vector
-fit :: Network -> Vector R -> Network
+-- net -> feature vector
+fit :: Network -> Matrix R -> Network
 fit n x = map (\(layer, idx) -> Layer {weights=initWeights (sz layer) (len idx), biases=initBiases (sz layer), sz=sz layer, activation=activation layer}) $ enumerate n
     where
-        len idx = if idx == 0 then size x else sz $ n!!(idx - 1)
+        len idx = if idx == 0 then fst (size x) else sz $ n!!(idx - 1)
 
-forwardProp :: Network -> Vector R -> Vector R
-forwardProp n x = forwardPropHelper x (length n)
+forwardProp :: Network -> Matrix R -> [Matrix R]
+forwardProp n x = scanl (flip activate) x n
+
+-- output is from previous layer (AKA the input to this layer)
+-- sigmoid'(input) is sigmoid(input) * (1 - sigmoid(input)) so we can substitute that in place
+calculateDelta :: (Matrix R, Layer) -> Matrix R -> Matrix R
+calculateDelta (output, nextL) deltaNext = (output * (1 - output)) * tr (weights nextL) LA.<> deltaNext
+
+-- `outputs` comes right from forwardProp
+-- Return a list of tuples with weights and biases for every layer
+backProp :: Network -> [Matrix R] -> Matrix R -> [(Matrix R, Matrix R)]
+backProp n outputs target = zip dWs dBs
     where
-        forwardPropHelper :: Vector R -> Int -> Vector R
-        forwardPropHelper input s =    
-            if s == 0 
-                then input 
-            else forwardPropHelper (activate (n!!(length n - s)) input) (s - 1)
+        outputError = cmap sigmoid' (target - last outputs)
+
+        reversedOutputs = tail $ reverse outputs
+        reversedNextLayers = reverse $ tail n
+        
+        layersWithOutputs = zip reversedOutputs reversedNextLayers
+        deltas = reverse $ scanl (flip calculateDelta) outputError layersWithOutputs 
+
+        dWs = [delta LA.<> tr out | (delta, out) <- zip (init deltas) (init outputs)]
+        dBs = deltas
--- src/Util.hs
+++ src/Util.hs
@@ -1,7 +1,26 @@
 module Util
     ( enumerate
+    , sigmoid
+    , sigmoid'
+    , mse
     )
 where
 
+import Numeric.LinearAlgebra
+
 enumerate :: [a] -> [(a, Int)]
 enumerate a = zip a [0..length a]
+
+sigmoid :: R -> R
+sigmoid x = 1 / (1 + exp (-x))
+
+sigmoid' :: R -> R
+sigmoid' x = sigmoid x * (1 - sigmoid x)
+
+
+-- vector of predictions -> y vector
+mse :: Vector R -> Vector R -> R
+mse yHatVector yVector = (1/(2*n)) * totalLoss
+    where 
+        totalLoss = sum $ toList $ (yHatVector - yVector) ** 2
+        n = fromIntegral (size yHatVector) :: R
