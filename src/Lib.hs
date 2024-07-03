de4cf145fe94f11f19b85bedbec006f5ba3d5a41
vlimki <vlimki@proton.me>
2024-07-03 11:26:21 +0300

    batch gradient descent; succesfully predict XOR but full of issues

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,15 +9,19 @@ module Lib
     , biases
     , train
     , predict
-        , updateParams
-        , trainStep
-        , forwardProp
-        , backProp
+    , updateParams
+    , trainStep
+    , forwardProp
+    , backProp
     ) where
 
 import Numeric.LinearAlgebra
 import qualified Numeric.LinearAlgebra as LA
-import Util (enumerate, sigmoid, sigmoid')
+import Util (enumerate, sigmoid, sigmoid', matrixToRows)
+import Data.List (transpose)
+import System.Random (randomRIO)
+import Control.Monad (replicateM)
+
 
 data Layer = Layer
     { weights :: Matrix R
@@ -26,25 +30,40 @@ data Layer = Layer
     , activation :: R -> R
     }
 
+-- 1. Try prev example
+-- 2. If it doesn't work fix it
+-- matrixToRows is broken
+-- 
 type Network = [Layer]
-
-initWeights :: Int -> Int -> Matrix R
-initWeights n m = (n><m) $ replicate (n*m) 0.123
-
-initBiases :: Int -> Matrix R
-initBiases n = (n><1) $ replicate n 0.123
 
 activate :: Layer -> Matrix R -> Matrix R
 activate l x = cmap (activation l) (weights l LA.<> x + biases l)
 
-initialize :: [Int] -> (R -> R) -> Network
-initialize s f = map (\x -> Layer {weights=initWeights 0 0, biases=initBiases 0, sz=x, activation=f}) s
+initWeights :: Int -> Int -> IO (Matrix R)
+initWeights n m = do
+    values <- replicateM (n * m) (randomRIO (-0.1, 0.1))
+    return $ (n><m) values
 
--- net -> feature vector
-fit :: Network -> Matrix R -> Network
-fit n x = map (\(layer, idx) -> Layer {weights=initWeights (sz layer) (len idx), biases=initBiases (sz layer), sz=sz layer, activation=activation layer}) $ enumerate n
+-- Initialize biases with small random values
+initBiases :: Int -> IO (Matrix R)
+initBiases n = do
+    values <- replicateM n (randomRIO (-0.1, 0.1))
+    return $ (n><1) values
+
+initialize :: [Int] -> (R -> R) -> IO Network
+initialize s f = mapM (\x -> do
+    w <- initWeights 0 0
+    b <- initBiases 0
+    return Layer {weights = w, biases = b, sz = x, activation = f}) s
+
+fit :: Network -> Matrix R -> IO Network
+fit n x = mapM (\(layer, idx) -> do
+    w <- initWeights (sz layer) (len idx)
+    b <- initBiases $ sz layer
+    return Layer {weights = w, biases = b, sz = sz layer, activation = activation layer}) (enumerate n)
     where
         len idx = if idx == 0 then fst (size x) else sz $ n!!(idx - 1)
+
 
 forwardProp :: Network -> Matrix R -> [Matrix R]
 forwardProp n x = scanl (flip activate) x n
@@ -52,30 +71,40 @@ forwardProp n x = scanl (flip activate) x n
 -- output is from previous layer (AKA the input to this layer)
 -- sigmoid'(input) is sigmoid(input) * (1 - sigmoid(input)) so we can substitute that in place
 calculateDelta :: (Matrix R, Layer) -> Matrix R -> Matrix R
-calculateDelta (output, nextL) deltaNext = tr (weights nextL) LA.<> deltaNext * cmap sigmoid' output
+calculateDelta (output, nextL) deltaNext = (tr (weights nextL) LA.<> deltaNext) * cmap sigmoid' output
 
 -- `outputs` comes right from forwardProp
 -- Return a list of tuples with weights and biases for every layer
 backProp :: Network -> [Matrix R] -> Matrix R -> [(Matrix R, Matrix R)]
-backProp n outputs target = zip dWs dBs
+backProp n outputs target = zip dW dB
     where
         outputError = (last outputs - target) * cmap sigmoid' (last outputs)
-
         reversedOutputs = tail $ reverse outputs
         reversedNextLayers = reverse $ tail n
 
         layersWithOutputs = zip reversedOutputs reversedNextLayers
         deltas = reverse $ scanl (flip calculateDelta) outputError layersWithOutputs
 
-        dWs = [delta LA.<> tr out | (delta, out) <- zip deltas outputs]
-        dBs = deltas
+        dW = [delta LA.<> tr out | (delta, out) <- zip deltas (init outputs)]
+        dB = deltas
 
 -- Network -> [(weight matrix for the l:th layer, bias vector (n x 1 matrix) for the l:th layer
 updateParams :: Network -> [(Matrix R, Matrix R)] -> Network
-updateParams = zipWith (curry (\(l, (dW, dB)) -> Layer {weights=weights l - dW * 0.01, biases=biases l - dB * 0.01, sz=sz l, activation=activation l}))
+updateParams = zipWith (curry (\(l, (dW, dB)) -> Layer {weights=weights l - dW * 10, biases=biases l - dB * 10, sz=sz l, activation=activation l}))
 
+-- Batch gradient descent
+-- Train on every input and output vector and take the average of all the backpropped weights and apply them
 trainStep :: Network -> Matrix R -> Matrix R -> Network
-trainStep n x y = updateParams n $ backProp n (forwardProp n x) y
+trainStep n x y = updateParams n totalParams
+    where
+        inputs = matrixToRows x
+        outputs = matrixToRows y
+
+        -- `gradients` is of type [[(Matrix R, Matrix R)]]. The outer array is for the training examples and the inner one is for the layers.
+        gradients = [backProp n (forwardProp n input) output | (input, output) <- zip inputs outputs]
+        totalParams = map f $ transpose gradients
+        f gs = let len = fromIntegral (length gs) in foldl1 (\(accW, accB) (w, b) -> (accW + w / len, accB + b / len)) gs
+        --params = map (\(a, b) -> (a / fromIntegral (length inputs), b / fromIntegral (length inputs))) totalParams
 
 train :: Network -> Matrix R -> Matrix R -> Int -> Network
 train n _ _ 0 = n
--- src/Util.hs
+++ src/Util.hs
@@ -3,6 +3,9 @@ module Util
     , sigmoid
     , sigmoid'
     , mse
+    , matrixToRows
+    , xorInput
+    , xorOutput
     )
 where
 
@@ -16,12 +19,30 @@ sigmoid x = 1 / (1 + exp (-x))
 
 -- Assuming the argument has alreadh been "sigmoided"
 sigmoid' :: R -> R
-sigmoid' x = x * (1 - x)
+sigmoid' x = sigmoid x * (1 - sigmoid x)
 
 
 -- vector of predictions -> y vector
 mse :: Vector R -> Vector R -> R
 mse yHatVector yVector = (1/(2*n)) * totalLoss
-    where 
+    where
         totalLoss = sum $ toList $ (yHatVector - yVector) ** 2
         n = fromIntegral (size yHatVector) :: R
+
+-- Converts a matrix into column vectors from its rows
+matrixToRows :: Matrix R -> [Matrix R]
+matrixToRows x = map tr $ map asRow $ toRows x
+
+xorInput :: Matrix R
+xorInput = (4><2)
+    [ 0, 0
+    , 1, 0
+    , 0, 1
+    , 1, 1] :: Matrix R
+            
+xorOutput :: Matrix R
+xorOutput = (4><1)
+    [ 0
+    , 1
+    , 1
+    , 0 ]
